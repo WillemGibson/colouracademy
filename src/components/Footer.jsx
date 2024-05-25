712d9512a8e69f74117d87121432b3743fba6917
Willem Gibson <129262358+WillemGibson@users.noreply.github.com>
2024-05-25 11:56:30 +1000

    feat: footer styling implemented

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import { DarkModeToggleButton } from "./DarkModeToggleButton";
+import "../styles/footer.css"
 
 
 export default function Footer(){
 
 	return (
 		<footer>
-			<h6>Copyright 2024 ColourAcademy / Alex</h6>
+			<h6>Copyright 2024 ColourAcademy</h6>
 			<a href="mailto:test@email.com">Email us!</a>
 
 			<DarkModeToggleButton />
